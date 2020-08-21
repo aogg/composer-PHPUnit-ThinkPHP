0d74733b85ffe10ce6a97c76ab099956c6b7004f
aogg <aogg@users.noreply.github.com>
2020-08-21 16:31:09 +0800

    getRequestUrlStringByArr

--- src/BaseTestCase.php
+++ src/BaseTestCase.php
@@ -12,13 +12,32 @@ abstract class BaseTestCase extends \PHPUnit\Framework\TestCase
     use traits\CrawlerTrait,
         traits\AssertTrait;
 
-
-    protected function getApiUrlString(string $url = '', array $vars = [])
+    protected function getRequestUrlString(string $url = '', array $vars = [], $prefix = '/api/')
     {
         return url(
-            '/api/' . ltrim($url, '/'), $vars, false,
+            rtrim($prefix, '/') . '/' . ltrim($url, '/'), $vars, false,
             parse_url(config('app.app_host'), PHP_URL_HOST)
         )->build();
+    }
+
+    /**
+     * 简化版获取url
+     *
+     * @param string|array $url 数组或者字符串
+     * @param string $prefix
+     * @return string
+     */
+    protected function getRequestUrlStringByArr($url, $prefix = '/api/')
+    {
+        if (is_string($url)) {
+            return $this->getRequestUrlString($url, [], $prefix);
+        }
+
+        return $this->getRequestUrlString(
+            isset($url[0])?$url[0]:'',
+            isset($url[1])?$url[1]:[],
+            isset($url[2])?$url[2]:$prefix,
+        );
     }
 
     protected function prepareUrlForRequest($uri)
